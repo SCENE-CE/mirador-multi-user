f3aed9bd922b71f19cbb51e151d8688da09ec954
Antoine <antoine.roy@tetras-libre.fr>
2024-04-18 16:46:34 +0200

    wip mirador integration into react app

---
+++ frontend/mirador.d.ts
@@ -0,0 +1,6 @@
+declare module "mirador-module" {
+  import { ReactElement } from "react";
+
+  function mirador():ReactElement;
+  export default mirador;
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <p>Hello Tetras !</p>
+      <div id='demo'></div>
+      <script src='Mirador/mirador-annotations-editor.ts'></script>
     </div>
   );
 }
