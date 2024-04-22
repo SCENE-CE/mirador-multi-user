78c653a788e3ee7f02b9248f4630347664714d01
Antoine <antoine.roy@tetras-libre.fr>
2024-04-22 11:31:05 +0200

    set up dockerisation with vite app

--- frontend-TS/vite.config.ts
+++ frontend-TS/vite.config.ts
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:true,
+    port:4000,
+  },
   resolve: {
     alias: {
       'mirador-annotation-editor/annotationAdapter/LocalStorageAdapter':
         'path/to/mirador-annotation-editor/annotationAdapter/LocalStorageAdapter'
     }
+
   }
 })
