fee6eb481fd6b7e989bcb3dc60d4d7f17201f376
Antoine <antoine.roy@tetras-libre.fr>
2024-04-24 10:41:02 +0200

    Guard route implementation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { loginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
 
 //TODO: Implement Validators fo Params
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: loginDto) {
     return this.authService.signIn(signInDto.id, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getprofile(@Request() req) {
+    return req.user;
   }
 }
