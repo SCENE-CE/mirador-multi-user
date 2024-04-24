3a852a73ce0d07157fbf5fdb87fbf291f2e13339
Antoine <antoine.roy@tetras-libre.fr>
2024-04-24 10:20:21 +0200

    JWT on signin route

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './src/config/.env.dev',
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+    UsersModule,],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,38 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from '../users/users.service';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
-  async signIn(id: number, pass: string): Promise<any> {
+
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    ) {}
+
+  async signIn(
+    id: number,
+    pass: string
+  ): Promise<{ access_token: string }> {
+    console.log("enter signIn", id);
     const user = await this.usersService.findOne(id);
+    console.log(user)
     if (user?.password !== pass) {
+      console.log('error')
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload = {sub:user.id, user: user.name};
+    console.log(payload)
+    const toto = {
+      access_token: await this.jwtService.signAsync(payload),
+    }
+    console.log("access_token :",toto)
+
+    return{
+
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
+
+
 }
