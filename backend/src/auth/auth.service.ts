1a8fb46cd8baf925e5693c35c15b22c6705085ef
Antoine <antoine.roy@tetras-libre.fr>
2024-04-24 10:42:23 +0200

    Cleaning code from console.log

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,38 +1,23 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-import { JwtService } from "@nestjs/jwt";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-    ) {}
+  ) {}
 
-  async signIn(
-    id: number,
-    pass: string
-  ): Promise<{ access_token: string }> {
-    console.log("enter signIn", id);
+  async signIn(id: number, pass: string): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(id);
-    console.log(user)
     if (user?.password !== pass) {
-      console.log('error')
       throw new UnauthorizedException();
     }
-    const payload = {sub:user.id, user: user.name};
-    console.log(payload)
-    const toto = {
-      access_token: await this.jwtService.signAsync(payload),
-    }
-    console.log("access_token :",toto)
+    const payload = { sub: user.id, user: user.name };
 
-    return{
-
+    return {
       access_token: await this.jwtService.signAsync(payload),
     };
   }
-
-
 }
