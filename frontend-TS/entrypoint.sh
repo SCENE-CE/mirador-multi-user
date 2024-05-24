192a9dd3a11c40e1d3ea82bf9ad0b7999d702b1a
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-05-24 10:52:11 +0200

    Fix import config.ts

--- frontend-TS/entrypoint.sh
+++ frontend-TS/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-if [ ${ENV} = "PROD" ]; then
 
+rm ./src/config.ts
+cp ./src/config.ts.example ./src/config.ts
+sed -i "s/SED_REPLACEMENT_BACKEND_URL/${BACKEND_URL}/g" ./src/config.ts
+
+if [ ${ENV} = "PROD" ]; then
     echo "Starting the application in production mode"
     serve ./dist -p 4000
 else
