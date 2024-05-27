eca3161b4fa6be3a380adc59511651fd4951b3c3
Antoine <antoine.roy@tetras-libre.fr>
2024-05-27 16:19:08 +0200

    fix 404 error on wrong url and cleaner docker file set up

--- frontend-TS/entrypoint.sh
+++ frontend-TS/entrypoint.sh
@@ -7,7 +7,7 @@ sed -i "s/SED_REPLACEMENT_BACKEND_URL/${BACKEND_URL}/g" ./src/config.ts
 
 if [ ${ENV} = "PROD" ]; then
     echo "Starting the application in production mode"
-    serve ./dist -p 4000
+    serve ./dist -p 4000 --single
 else
     echo "Starting the application in dev mode"
     npm run dev
