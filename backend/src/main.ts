0082b0391463034be3b76e39015db4e3429c79c9
Antoine <antoine.roy@tetras-libre.fr>
2024-07-01 14:33:09 +0200

    clear console.log

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-console.log('dbName', process.env.DB_DATABASE);
-console.log('dbhost', process.env.DB_HOST);
-console.log('dbPort', process.env.DB_PORT);
-console.log('dbusername', process.env.DB_USER);
-console.log('dbpassword', process.env.DB_PASS);
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
