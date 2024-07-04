99a0fba12b8705f7c65f410a783a55deb28cef06
Antoine <antoine.roy@tetras-libre.fr>
2024-07-04 16:22:41 +0200

    create link group project dto

--- backend/src/link-group-project/dto/create-link-group-project.dto.ts
+++ backend/src/link-group-project/dto/create-link-group-project.dto.ts
@@ -1,1 +1,19 @@
-export class CreateLinkGroupProjectDto {}
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { GroupProjectRights } from '../../enum/group-project-rights';
+import { CreateProjectDto } from '../../project/dto/create-project.dto';
+import { Type } from 'class-transformer';
+import { CreateUserGroupDto } from '../../user-group/dto/create-user-group.dto';
+
+export class CreateLinkGroupProjectDto {
+  @IsEnum(GroupProjectRights)
+  @IsNotEmpty()
+  GroupProjectRights: GroupProjectRights;
+
+  @Type(() => CreateProjectDto)
+  @IsOptional()
+  Projects: CreateProjectDto[];
+
+  @Type(() => CreateUserGroupDto)
+  @IsOptional()
+  UserGroups: CreateUserGroupDto[];
+}
