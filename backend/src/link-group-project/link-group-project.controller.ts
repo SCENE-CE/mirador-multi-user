82805fcd9b347d9abfd39ba36c19596342c919e6
Antoine <antoine.roy@tetras-libre.fr>
2024-07-09 10:34:28 +0200

    userGuard authentification

--- backend/src/link-group-project/link-group-project.controller.ts
+++ backend/src/link-group-project/link-group-project.controller.ts
@@ -1,59 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Put,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { LinkGroupProjectService } from './link-group-project.service';
-import { CreateLinkGroupProjectDto } from './dto/create-link-group-project.dto';
-import { UpdateLinkGroupProjectDto } from './dto/update-link-group-project.dto';
 
 @Controller('link-group-project')
 export class LinkGroupProjectController {
   constructor(
     private readonly linkGroupProjectService: LinkGroupProjectService,
   ) {}
-
-  @Post()
-  create(@Body() createLinkGroupProjectDto: CreateLinkGroupProjectDto) {
-    return this.linkGroupProjectService.create(createLinkGroupProjectDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.linkGroupProjectService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.linkGroupProjectService.findOne(+id);
-  }
-
-  @Get('/project/:id')
-  async findAllUserGroup(@Param('id') id: number) {
-    console.log('findAllProject SERVICE');
-    return await this.linkGroupProjectService.findAllGroupByProjectId(id);
-  }
-
-  @Get('user-group/:id')
-  async findAllProject(@Param('id') id: number) {
-    console.log('findAllUserGroup SERVICE');
-    return await this.linkGroupProjectService.findAllProjectByUserGroupId(id);
-  }
-
-  @Put(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateLinkGroupProjectDto: UpdateLinkGroupProjectDto,
-  ) {
-    return this.linkGroupProjectService.update(+id, updateLinkGroupProjectDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.linkGroupProjectService.remove(+id);
-  }
 }
--- backend/src/link-media-group/link-media-group.controller.ts
+++ backend/src/link-media-group/link-media-group.controller.ts
@@ -1,56 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Put,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { LinkMediaGroupService } from './link-media-group.service';
-import { CreateLinkMediaGroupDto } from './dto/create-link-media-group.dto';
-import { UpdateLinkMediaGroupDto } from './dto/update-link-media-group.dto';
 
 @Controller('link-media-group')
 export class LinkMediaGroupController {
   constructor(private readonly linkMediaGroupService: LinkMediaGroupService) {}
-
-  @Post()
-  create(@Body() createLinkMediaGroupDto: CreateLinkMediaGroupDto) {
-    return this.linkMediaGroupService.create(createLinkMediaGroupDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.linkMediaGroupService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.linkMediaGroupService.findOne(+id);
-  }
-
-  @Get('user-group/:id')
-  async findAllMediaByUserGroupId(@Param('id') id: number) {
-    return await this.linkMediaGroupService.findAllMediaByUserGroupId(id);
-  }
-
-  @Get('media/:id')
-  async findAllUserGroupByMediaId(@Param('id') id: number) {
-    return await this.linkMediaGroupService.findAllUserGroupByMediaId(id);
-  }
-
-  @Put(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateLinkMediaGroupDto: UpdateLinkMediaGroupDto,
-  ) {
-    return this.linkMediaGroupService.update(+id, updateLinkMediaGroupDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.linkMediaGroupService.remove(+id);
-  }
 }
