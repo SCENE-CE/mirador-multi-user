25bc3ad219d8b8143a2bd035d9dc8de6138f898e
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-09-24 14:35:56 +0200

    Add Swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as Process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle(Process.env.SWAGGER_TITLE)
+    .setDescription(Process.env.SWAGGER_DESCRIPTION)
+    .setVersion(Process.env.SWAGGER_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(Process.env.SWAGGER_RELATIVE_PATH, app, document);
+
+  await app.listen(Process.env.BACKEND_PORT);
 }
 bootstrap();
