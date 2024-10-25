06a3d5cf5a643c97b91d15ad566fc6eeb5c0e673
Drilio <Roy-Antoine93@proton.me>
2024-10-25 14:26:29 +0200

    tag and tagging entity and service creation

--- backend/src/BaseEntities/tag/tag.service.ts
+++ backend/src/BaseEntities/tag/tag.service.ts
@@ -1,25 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
-import { Tag } from "./entities/tag.entity";
-import { Repository } from "typeorm";
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { Repository } from 'typeorm';
+import { CustomLogger } from '../../utils/Logger/CustomLogger.service';
 
 @Injectable()
 export class TagService {
+  private readonly logger = new CustomLogger();
+
   constructor(
     @InjectRepository(Tag)
     private readonly tagRepository: Repository<Tag>,
   ) {}
 
   async createTag(name: string, isCustom = false): Promise<Tag> {
-    let tag = await this.tagRepository.findOne({ where: { name } });
-    if (!tag) {
-      tag = this.tagRepository.create({ name, isCustom });
-      await this.tagRepository.save(tag);
+    try {
+      const tag = this.tagRepository.create({ name, isCustom });
+
+      await this.tagRepository.upsert(tag, ['name']);
+
+      return this.tagRepository.findOneOrFail({ where: { name } });
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      throw new InternalServerErrorException(
+        'An error occurred while creating the tag',
+        error,
+      );
     }
-    return tag;
   }
 
   async getAllTags(): Promise<Tag[]> {
-    return this.tagRepository.find();
+    try {
+      return this.tagRepository.find();
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      throw new InternalServerErrorException(
+        'An error occurred while finding the tags',
+        error,
+      );
+    }
   }
 }
--- backend/src/LinkModules/tagging/tagging.module.ts
+++ backend/src/LinkModules/tagging/tagging.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TaggingService } from './tagging.service';
 import { TaggingController } from './tagging.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tag } from '../../BaseEntities/tag/entities/tag.entity';
+import { Tagging } from './entities/tagging.entity';
+import { TagModule } from '../../BaseEntities/tag/tag.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tagging]), TagModule],
   controllers: [TaggingController],
   providers: [TaggingService],
+  exports: [TaggingService],
 })
 export class TaggingModule {}
