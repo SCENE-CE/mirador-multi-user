7e61b97455172dd7af8225b8052cf9c5c293b451
Drilio <Roy-Antoine93@proton.me>
2024-11-13 08:47:59 +0100

    improve ui

--- frontend-TS/src/features/auth/routes/Register.tsx
+++ frontend-TS/src/features/auth/routes/Register.tsx
@@ -1,9 +1,22 @@
 import { RegisterForm } from "../components/RegisterForm.tsx";
 import { Layout } from "../components/layout.tsx";
+import { Grid, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export const Register = ()=>{
     return(
-      <Layout title="Create your account">
+      <Layout
+        title="Create your account"
+        rightButton={
+            <Grid>
+              <NavLink to="/auth/login">
+                <Typography variant="button">
+                  LOGIN
+                </Typography>
+              </NavLink>
+            </Grid>
+        }
+      >
         <RegisterForm />
       </Layout>
     )
