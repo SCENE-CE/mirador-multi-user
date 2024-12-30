fcaefee44a6db13680c1fbca8057ddd2181c04ae
Drilio <Roy-Antoine93@proton.me>
2024-12-30 14:45:12 +0100

    extend mimetype accepted

--- backend/src/BaseEntities/media/utils/fileFilterMedia.ts
+++ backend/src/BaseEntities/media/utils/fileFilterMedia.ts
@@ -1,7 +1,7 @@
 import { UnsupportedMediaTypeException } from '@nestjs/common';
 
 export const fileFilterMedia = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|webp|gif)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|webp|gif|bmp|tiff|svg|ico|jfif|heic|heif)$/i)) {
     return callback(new UnsupportedMediaTypeException('Only image files are allowed!'), false);
   }
   callback(null, true);
